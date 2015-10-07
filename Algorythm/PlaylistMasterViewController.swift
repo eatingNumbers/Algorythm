ba5152b3f00b5e7c7df5340e2215e908a85f12a2
Dean Laurea <deanlaurea@icloud.com>
2015-10-07 14:59:52 -0500

    Fix issue and add function to override default in prepareForSegue

--- Algorythm/PlaylistMasterViewController.swift
+++ Algorythm/PlaylistMasterViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PlaylistMasterViewController.swift
 //  Algorythm
 //
 //  Created by Dean Laurea on 10/6/15.
@@ -15,16 +15,19 @@ class PlaylistMasterViewController: UIViewController {
         super.viewDidLoad()
         
         aButton.setTitle("Press me", forState: .Normal)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-    @IBAction func buttonPressed() {
-        view.backgroundColor = UIColor.orangeColor()
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showPlaylistDetail" {
+            let playlistDetailController = segue.destinationViewController as! PlaylistDetailViewController
+            playlistDetailController.segueLabelText = "Yay! You pressed the button!"
+        }
     }
 }
 
--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 ##Working With Multiple View Controllers
 ####What's the reason you couldn't view the different sizes on the storyboard?
 Because the tutorial called for unchecking the "Use Size Classes" box in the Utilities panel under "File Inspector". 
+
+##Using a Navigation Controller With Segues
+####How do you add a navigation controller to your app?
+
+####How do you add a title on the navigation bar of your views?
