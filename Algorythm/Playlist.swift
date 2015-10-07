870e7f54a76b943c5a644df68bdc5ac9333b7c97
Dean Laurea <deanlaurea@icloud.com>
2015-10-07 18:05:10 -0500

    Add MusicLibrary and Playlist files and populate

---
+++ Algorythm/Playlist.swift
@@ -0,0 +1,21 @@
+//
+//  Playlist.swift
+//  Algorythm
+//
+//  Created by Dean Laurea on 10/7/15.
+//  Copyright © 2015 Dean Laurea. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Playlist {
+    
+    var title: String?
+    var description: String?
+    var icon: UIImage?
+    var largeIcon: UIImage?
+    var artists: [String] = []
+    var backgroundColor: UIColor = UIColor.clearColor()
+    
+}
