95bb1c34e01ec8eb208c32b839757625acdc4e3d
Dean Laurea <deanlaurea@icloud.com>
2015-10-07 17:45:21 -0500

    Add images to Assets.xcassets

---
+++ Algorythm/PlaylistDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlaylistDetailViewController.swift
+//  Algorythm
+//
+//  Created by Dean Laurea on 10/6/15.
+//  Copyright © 2015 Dean Laurea. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistDetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var buttonPressLabel: UILabel!
+    var segueLabelText: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        buttonPressLabel.text = segueLabelText
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
